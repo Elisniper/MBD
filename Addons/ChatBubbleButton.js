1f0482691cd9f67ea98ea7ad088341747f670620
Elisniper <spyros270598@gmail.com>
2022-05-30 18:32:29 +1000

    Create ChatBubbleButton.js

---
+++ Addons/ChatBubbleButton.js
@@ -0,0 +1,4 @@
+document.body.innerHTML = '<button>Enable Chat Bubble</button>';
+
+var styles = window.document.styleSheets[0];
+StyleSheet.insertRule('.message-2CShn3.groupStart-3Mlgv1 {position: relative;margin: 16px 5px 0 70px;padding: 8px;background-color: var(--mbd-black1);border-bottom: none !important;overflow: visible !important;}', styles.cssRules.length); 
